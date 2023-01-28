d2ef6cbe400fa4c013b1e5a4592bc08fed97203c
Benjamin Ocampo <nicolasbenjaminocampo@gmail.com>
2023-01-28 18:41:57 +0100

    Add code structure

---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup, find_packages
+
+setup(name="ISHate",
+      description="An In-depth Analysis of Implicit and Subtle Hate Speech Messages",
+      author="Benjamin Ocampo",
+      author_email="nicolas.ocampo@inria.fr",
+      version="1.0",
+      license='BSL',
+      packages=find_packages())
